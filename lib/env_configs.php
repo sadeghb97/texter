856e857076785f5d53265ae8340e9b9ff3f2e0e1
Sadegh Bagherzadeh <sadeghb97@gmail.com>
2026-05-21 17:58:51 +0330

    fix avt public url

--- lib/env_configs.php
+++ lib/env_configs.php
@@ -3,6 +3,6 @@ const DB_HOST = "localhost";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_NAME = "texter";
-const AVT_PUBLIC_URL = "/puzzlinho";
+const AVT_PUBLIC_URL = "/texter";
 const AVT_ASSETS_URL = "/avetify/assets";
 ?>
