de17a3a55692b6f0e55dfff04197c3418911969b
Sadegh Bagherzadeh <sadeghb97@gmail.com>
2026-05-21 17:54:56 +0330

    improve library imports

--- lib/env_configs.php
+++ lib/env_configs.php
@@ -3,4 +3,6 @@ const DB_HOST = "localhost";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_NAME = "texter";
+const AVT_PUBLIC_URL = "/puzzlinho";
+const AVT_ASSETS_URL = "/avetify/assets";
 ?>
--- lib/library.php
+++ lib/library.php
@@ -1,7 +1,7 @@
 <?php
-require_once "AppConfigs.php";
-require_once "init_avt.php";
 require_once "env_configs.php";
+require_once "init_avt.php";
+require_once "AppConfigs.php";
 require_once "TexterConnection.php";
 require_once "TexterAuth.php";
 require_once "TexterAuthApiController.php";
